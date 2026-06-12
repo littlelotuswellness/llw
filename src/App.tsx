abeab99bb028db1ce776626bda94ff39cb739cf3
Little Lotus Wellness <littlelotuswellness@gmail.com>
2026-06-12 13:12:47 +1000

    Update time slots list to show hourly intervals from 9AM to 5PM

--- src/App.tsx
+++ src/App.tsx
@@ -558,11 +558,14 @@ function BookingModal({ onClose }: BookingModalProps) {
 
   const timeSlots = [
     '09:00 AM',
-    '10:30 AM',
+    '10:00 AM',
+    '11:00 AM',
     '12:00 PM',
-    '01:30 PM',
+    '01:00 PM',
+    '02:00 PM',
     '03:00 PM',
-    '04:30 PM'
+    '04:00 PM',
+    '05:00 PM'
   ];
 
   const today = new Date().toISOString().split('T')[0];
